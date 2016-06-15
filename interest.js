051f61c7e10d838072915df490f3bbcad53b69d9
Robert Bays <bays.robert@gmail.com>
2016-06-15 10:22:42 +0100

    Made the interest calculator work
    YAY!!!!

--- interest.js
+++ interest.js
@@ -1,7 +1,47 @@
-var capital = 200.0;
-var apr = 10.0;
-var years = 0.5;
+var capital = 0;
+var apr = 0;
+var years = 0;
 
-var interestDec = 1.0 + apr / 100;
-var answer = capital * Math.pow(interestDec,years);
-document.getElementById("result").innerHTML = answer;
+
+
+var slider = document.getElementById("lengthMonth");
+var lengthWords = document.getElementById("lengthWords");
+var submitButton = document.getElementById("button1");
+
+var resultsDiv = document.getElementById("result");
+
+
+//update length on slider move
+slider.addEventListener("input",lengthUpdate,false);
+
+//submit button to update result
+submitButton.addEventListener('click',submit,false);
+
+//updates the length of the loan in the DOM
+function lengthUpdate(){
+	
+	var totalMonths = slider.value;
+	var years = Math.floor(totalMonths / 12);
+	var months = totalMonths % 12;
+	lengthWords.innerHTML = "Length: " + years + " years and " + months + " month(s)";
+	window.years = totalMonths / 12; //updates term of loan in years
+}
+
+//submit all values to get repayment amount
+function submit(){
+	window.capital = document.getElementById("pounds").value;
+	window.apr = document.getElementById("apr").value;
+	if(capital === 0 || apr === 0 || years === 0){
+		resultsDiv.innerHTML = "One or more missing or zero values.<br>please enter values above.<br> capital: " + capital + "<br>apr: " + apr + "<br>years: " + years;
+	}
+	else{
+		calculateRepayment();
+	}
+}
+//calculates the total repayment from the 3 values given
+function calculateRepayment(){
+	var interestDec = 1.0 + apr / 100;
+	var answer = capital * Math.pow(interestDec,years);
+	answer = Math.round(answer * 100) / 100;
+	resultsDiv.innerHTML = "You will need to repay £" + answer;
+}
